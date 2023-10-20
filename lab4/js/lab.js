283ff68b777db95d85701cbea0ae77c308fc10ce
Wing-k <146971131+Wing-k@users.noreply.github.com>
2023-10-20 16:08:52 -0700

    Lab 4 assignment changes
    Added pseudocode for making guacamole and connect 4 game to index.html and javascript, added images for assignment, changed css for website.

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Connect 4 
+// A game where the players choose a color and then take turns dropping colored tokens 
+// into a six by seven grid
+// Author: Ashley Hom & Cho Wing Koon
+// Date: October 23 2023
+
+// Pseudocode
+// Print a 6 X 7 grid with a circle in the middle of each squares
+// Accept user input for color
+  // Asks user if they want to be red or yellow
+// Accept user input for move
+// Validate user move
+// Evaluate possible moves
+// Choose computer move
+// Test of win or lose condition
+  // End game if 4 of the same color circles are connected in a vertical, horizontal, or diagonal row
+    // Player of that color wins
+// Repeat if win or lose condition is not met
+
 
 // Constants
 
