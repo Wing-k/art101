ba7a9019a5049c3a9a24bb2d13f18f9d731409a0
Wing-k <146971131+Wing-k@users.noreply.github.com>
2023-10-27 15:50:53 -0700

    publishing lab 5 and lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/* index.js - Lab 5 Javascript
+ * Author: Ashley Hom and Cho Wing Koon
+ * Created: 27 October
+ * License: Public Domain
+*/
 
-// Constants
+// Define variables
+var myTransport = ["legs", "bus", "bike", "car"];
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+var myMainRide = 
+{
+  make: "Mercedes-Benz",
+  model: "A-Class",
+  color: "white",
+  year: 2022,
+  age: function() {
+      return 2023 - this.year;
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// Outputs
+document.writeln("Getting around using: " + myTransport + "<br>");
 
-// let's get this party started
-main();
+// Allow us to write objects to the document
+document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
