102a7d62cb482e86cc7a5de51dc4e9bf5e2053b4
Wing-k <146971131+Wing-k@users.noreply.github.com>
2023-11-12 13:12:10 -0800

    lab 9

---
+++ lab8/js/lab.js
@@ -0,0 +1,44 @@
+/* index.js - Lab 8 Javascript
+ * Author: Ashley Hom and Cho Wing Koon
+ * Created: 3 November
+ * License: Public Domain
+*/
+
+function square(x){
+    var results = x * x
+    return results;
+}
+
+// test function
+console.log("What is the square of 7?", square(7));
+console.log("What is the square of -12?", square(-12));
+
+var randomArray = [2, 16, 21, 5, 58, 9]
+console.log("My array:", randomArray);
+
+var squaredResult = randomArray.map(square);
+// should return [4, 256, 441, 25, 3364, 81]
+console.log("Squaring the values of the array:", squaredResult);
+
+
+var results = randomArray.map(function(x){
+    return x + 5;
+})
+// should return [7, 21, 26, 10, 63, 14]
+console.log("Adding 5 to each value of the array:", results);
+
+function division(a, b){
+    divResult = a / b;
+}
+
+function callback(para){
+    para();
+}
+
+callback(function(){
+    var x = window.prompt("Give a number for x:");
+    var y = window.prompt("Give a number for y:");
+    division(x, y)
+})
+
+console.log("Dividing x by y:", divResult);
